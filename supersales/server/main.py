bc6d97b3a7a1bd35d54849a6f63e0f9b42abe34d
Sidharth Shah <sidharth@fafadiatech.com>
2011-04-28 16:27:24 +0530

    Added controller to handle authentication

--- supersales/server/main.py
+++ supersales/server/main.py
@@ -20,12 +20,35 @@ class AddUser(webapp.RequestHandler):
 			user.put()
 
 			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.out.write("Success")
+			self.response.out.write("1")
 		except:
 			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.out.write("Something is wrong with the request")
+			self.response.out.write("-1")
 
-application = webapp.WSGIApplication([('/',MainPage),('/users/add',AddUser)],debug=True)
+class AuthUser(webapp.RequestHandler):
+	def post(self):
+		try:
+			uemail = self.request.get('email')
+			upasswd = self.request.get('passwd')
+			
+			q = "WHERE email = '%s' AND passwd = '%s'" % (uemail,upasswd)
+
+			result = UserProfile.gql(q)
+			status = ""
+			
+			if(result.count() == 1):
+				status = "1"
+			else:
+				status = "0"
+
+			self.response.headers['Content-Type'] = 'text/plain'
+			self.response.out.write(status)
+						
+		except:
+			self.response.headers['Content-Type'] = 'text/plain'
+			self.response.out.write("-1")			
+
+application = webapp.WSGIApplication([('/',MainPage),('/users/add',AddUser),('/users/auth',AuthUser),],debug=True)
 
 def main():
 	run_wsgi_app(application)
