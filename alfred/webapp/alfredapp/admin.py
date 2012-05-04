a4e83477bd9377aefb1521c0c5a81224ac4e4e35
smitavk <smitak099@gmail.com>
2012-05-04 17:22:32 +0530

    add alfredapp in settings.py add models

---
+++ alfred/webapp/alfredapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin 
+from alfredapp.models import * 
+
+admin.site.register(Alfreduser)
+admin.site.register(Deparment)
+admin.site.register(TicketStatus)
+admin.site.register(Team)
+
+admin.site.register(Customer)
+admin.site.register(CustomerPackage)
+admin.site.register(Sla)
+admin.site.register(Dsk)
+admin.site.register(Make)
