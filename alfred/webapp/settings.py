1826d51d178c33d711433b5705ff9f3c8777da49
hardik kacha <hardikgkacha@gmail.com>
2012-05-04 16:18:21 +0530

    added img,css,js folder in static

--- alfred/webapp/settings.py
+++ alfred/webapp/settings.py
@@ -1,3 +1,4 @@
+import os
 # Django settings for webapp project.
 
 DEBUG = True
@@ -56,6 +57,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
 STATIC_ROOT = ''
 
 # URL prefix for static files.
@@ -69,6 +72,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT,"static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -103,6 +107,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'webapp.urls'
 
 TEMPLATE_DIRS = (
+    os.path.join(SITE_ROOT,"tpls"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
