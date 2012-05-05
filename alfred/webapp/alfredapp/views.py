a44122a1bb215e033471dbab266e3b4a407be52d
smitavk <smitak099@gmail.com>
2012-05-05 12:41:57 +0530

    add modify delete operation of team

--- alfred/webapp/alfredapp/views.py
+++ alfred/webapp/alfredapp/views.py
@@ -46,7 +46,44 @@ def deleteuser(request):
     pass
 
 def teamdashboard():
+    c = request.COOKIES.get('csrftoken','')
+    tpl = tpl_lookup.get_template("teamsummary.html")
+    team = Team.objects.all()
+    status = TicketStatus.objects.all()
+    return HttpResponse(tpl.render(csrf_token=c,on_home=True,status=status,team=team,userName='Smita'))
+
+
+def addteamform(request):
+    c = request.COOKIES.get('csrftoken','')
+    tpl = tpl_lookup.get_template("createteam.html")
+    user = Alfreduser.objects.all()
+    status = TicketStatus.objects.all()
+    return HttpResponse(tpl.render(csrf_token=c,on_home=True,status=status,User=user,userName='Smita'))
+
+def teamadd(request):
+    name = request.POST.get('name')
+    address = request.POST.get('address')
+    landlineno = request.POST.get('landlineno')
+    mobileno = request.POST.get('mobileno')
+    email = request.POST.get('email')
+    teamobj = Team(name=name,address=address,landlineno=landlineno,mobileno=mobileno,email=email)
+    teamobj.save()
+
+def teameditform(request):
+    c = request.COOKIES.get('csrftoken','')
+    tpl = tpl_lookup.get_template("editteam.html")
+    teamid = request.POST.get('teamid')
+    team = Team.objects.filter(id=teamid)
+    status = TicketStatus.objects.all()
+    return HttpResponse(tpl.render(csrf_token=c,on_home=True,team=team,status=status,User=user,userName='Smita'))
+   
+    
+def modifyteam(request):
+    pass 
+
+def deleteteam(request):
     pass
+
 
 
 def customersummary(request):
